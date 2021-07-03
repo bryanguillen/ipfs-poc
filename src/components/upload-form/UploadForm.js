aba17b21be017c47597727393da639dc995f90d0
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-03 15:22:23 -0400

    feat: add support for updating state when image is chosen This includes updating image name field as well as the internal state of form/app.

--- src/components/upload-form/UploadForm.js
+++ src/components/upload-form/UploadForm.js
@@ -1,13 +1,19 @@
 import './UploadForm.css';
 
-function UploadForm() {
+function UploadForm({
+  imageName,
+  onChange
+}) {
   return (
     <div className="upload-form">
-      <div className="upload-form-file-name">No Image Chosen</div>
+      <div className="upload-form-file-name">{imageName ? imageName : 'No Image Chosen'}</div>
       <div className="upload-form-browse-button-container">
         <button className="upload-form-button upload-form-browse-button">Browse Images</button>
         <input
+          accept="image/*"
           className="upload-form-file-input"
+          multiple={false}
+          onChange={onChange}
           type="file"
         />
       </div>
