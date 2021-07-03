cd62e3cb63274247eb24e52daed74e8b37e6f5b9
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-03 15:22:32 -0400

    feat: add support for updating state when image is chosen This includes updating image name field as well as the internal state of form/app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 import UploadForm from './components/upload-form/UploadForm'
 
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+
   return (
     <div className="app">
-      <UploadForm/>
+      <UploadForm
+        imageName={image ? image.name : ''}
+        onChange={(event) => setImage(event.target.files[0])}
+      />
     </div>
   );
 }
