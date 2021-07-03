f0c1c1dc0b83f2e4727d9af93e42be0d614816ee
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-03 18:31:50 -0400

    feat: add support for rendering different views within AppModal after submit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { create } from 'ipfs-http-client';
-import ImagePreview from './components/image-preview/ImagePreview';
+import AppModal from './components/app-modal/AppModal';
+import UploadFileSuccess from './components/upload-file-success/UploadFileSuccess'
 import UploadForm from './components/upload-form/UploadForm'
 import './App.css';
 
 function App() {
   const [image, setImage] = useState(null);
   const [imageUrl, setImageUrl] = useState('');
+  const [modalTitle, setModalTitle] = useState('Upload Image')
   const [submittingForm, setSubmittingForm] = useState(false);
   const [uploadFormError, setUploadFormError] = useState(false);
+  const [viewName, setViewName] = useState('');
+
+  const FILE_UPLOAD_SUCCESS = 'file-upload-success';
+
+  /**
+   * @description Effect for updating the view name when an image is uploaded;
+   * it's needed b/c we need the image url first, and since state updates
+   * can be async, this is the safest way of triggering the view update
+   */
+  useEffect(() => {
+    // Do not run on init
+    if (imageUrl) {
+      setViewName(FILE_UPLOAD_SUCCESS);
+      setModalTitle('File Upload Success');
+    }
+  }, [imageUrl]);
 
   /**
    * @description Function used to handle submit
@@ -17,15 +35,41 @@ function App() {
   async function onSubmit() {
     setSubmittingForm(true);
     uploadFileToIpfs()
-    .then(url => {
-        setSubmittingForm(false);
+      .then(url => {
         setImageUrl(url);
+        setSubmittingForm(false);
       })
       .catch(error => {
         console.log(error);
-        setSubmittingForm(false);
         setUploadFormError(true);
+        setSubmittingForm(false);
       });
+  }
+
+  /**
+   * @description Function used to render the correct view within the modal;
+   * it's essentially used to wrap the switch statement
+   * @returns {Object}
+   */
+  function renderView() {
+    switch(viewName) {
+      case FILE_UPLOAD_SUCCESS:
+        return (
+          <UploadFileSuccess
+            imageUrl={imageUrl}
+          />
+        )
+      default:
+        return (
+          <UploadForm
+            errorExists={uploadFormError}
+            imageName={image ? image.name : ''}
+            onChange={(event) => setImage(event.target.files[0])}
+            onSubmit={onSubmit}
+            submitting={submittingForm}
+          />
+        )
+    }
   }
 
   /**
@@ -45,20 +89,9 @@ function App() {
 
   return (
     <div className="app">
-      {
-        imageUrl ?
-            <ImagePreview
-              imageUrl={imageUrl}
-            />
-          :
-            <UploadForm
-              errorExists={uploadFormError}
-              imageName={image ? image.name : ''}
-              onChange={(event) => setImage(event.target.files[0])}
-              onSubmit={onSubmit}
-              submitting={submittingForm}
-            />
-      }
+      <AppModal title={modalTitle}>
+        {renderView()}
+      </AppModal>
     </div>
   );
 }
