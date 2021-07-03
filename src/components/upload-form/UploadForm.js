f0dcc5cf3d5568943160ccd3441aac78d89dcd59
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-03 15:14:26 -0400

    chore: update copy

--- src/components/upload-form/UploadForm.js
+++ src/components/upload-form/UploadForm.js
@@ -3,9 +3,9 @@ import './UploadForm.css';
 function UploadForm() {
   return (
     <div className="upload-form">
-      <div className="upload-form-file-name">No File Chosen</div>
+      <div className="upload-form-file-name">No Image Chosen</div>
       <div className="upload-form-browse-button-container">
-        <button className="upload-form-button upload-form-browse-button">Browse</button>
+        <button className="upload-form-button upload-form-browse-button">Browse Images</button>
         <input
           className="upload-form-file-input"
           type="file"
