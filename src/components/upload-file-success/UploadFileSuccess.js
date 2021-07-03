08c85a2258caba72e01f9efd5dfb34ad91035d88
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-03 18:41:11 -0400

    feat: remove copy URL functionality

--- src/components/upload-file-success/UploadFileSuccess.js
+++ src/components/upload-file-success/UploadFileSuccess.js
@@ -1,31 +1,17 @@
-import { useState } from 'react';
 import { IoCheckmarkCircleOutline } from 'react-icons/io5';
 import './UploadFileSuccess.css';
 
 function UploadFileSuccess({
-  imageUrl
+  imageUrl,
+  viewImage
 }) {
-  const [urlCopied, setUrlCopied] = useState(false);
-
-  /**
-   * @description Wrapper for whenever the URL is copied
-   * @returns {undefined}
-   */
-  function copyUrl() {
-    navigator.clipboard.writeText(imageUrl);
-    setUrlCopied(true);
-  }
-
   return (
     <div className="upload-file-success-container">
       <div className="upload-file-success-icon">
         <IoCheckmarkCircleOutline color={'green'} size={'7.5rem'}/>
       </div>
       <div className="upload-file-success-url">{imageUrl}</div>
-      <div className="upload-file-success-buttons-container">
-        <button className="button-secondary block-level-button" onClick={copyUrl}>Copy URL</button>
-        {urlCopied ? <button className="button-primary block-level-button">View File Button</button> : null}
-      </div>
+      <button className="button-primary block-level-button" onClick={viewImage}>View File</button>
     </div>
   )
 }
