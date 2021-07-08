33830d521b95a5e53bcb8c70dfa145bb3da8afe4
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-08 19:34:45 -0400

    feat: add btn for db interaction ui

--- src/components/intro-selections/IntroSelections.js
+++ src/components/intro-selections/IntroSelections.js
@@ -1,13 +1,14 @@
-import { IoCloudUpload } from 'react-icons/io5';
+import { IoCloudUpload, IoSave } from 'react-icons/io5';
 import './IntroSelections.css';
 
 function IntroSelections({
+  showDbInteractionUi,
   showUploadForm
 }) {
   return (
     <div className="intro-selections">
       <button
-        className="intro-selections-button intro-selections-upload-button"
+        className="intro-selections-button"
         onClick={showUploadForm}
       >
         <span className="intro-selections-icon">
@@ -16,6 +17,17 @@ function IntroSelections({
           />
         </span>
         <span>Upload Image</span>
+      </button>
+      <button
+        className="intro-selections-button intro-selections-button-db"
+        onClick={showDbInteractionUi}
+      >
+        <span className="intro-selections-icon">
+          <IoSave
+            size={'3rem'}
+          />
+        </span>
+        <span>Interact With DB</span>
       </button>
     </div>
   );
