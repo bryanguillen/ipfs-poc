9a743c70bfee031de5684bf7906290c97a575d04
Bryan Guillen <bguillen@truechoicesolutions.com>
2021-07-08 20:07:47 -0400

    feat: add stored people

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppModal from './components/app-modal/AppModal';
 import ImagePreview from './components/image-preview/ImagePreview';
 import IntroSelections from './components/intro-selections/IntroSelections';
 import PersonForm from './components/person-form/PersonForm';
+import StoredPeople from './components/stored-people/StoredPeople';
 import UploadFileSuccess from './components/upload-file-success/UploadFileSuccess'
 import UploadForm from './components/upload-form/UploadForm'
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
   const FILE_UPLOAD_SUCCESS = 'file-upload-success';
   const IMAGE_PREVIEW = 'image-preview';
   const PERSON_FORM = 'person-form';
+  const STORED_PEOPLE = 'stored-people';
   const UPLOAD_FORM = 'upload-form';
 
   /**
@@ -81,7 +83,13 @@ function App() {
         );
       case PERSON_FORM:
         return (
-          <PersonForm/>
+          <PersonForm
+            seeRecords={() => setModalData({ title: 'People', route: STORED_PEOPLE })}
+          />
+        );
+      case STORED_PEOPLE:
+        return (
+          <StoredPeople/>
         );
       default:
         return (
